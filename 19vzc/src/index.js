e36aa11b195bdb0c4a0f4dda18e7399603829d7a
schlucht <web@jagolo.ch>
2021-05-22 11:13:23 +0200

    Start Refactoring

--- 19vzc/src/index.js
+++ 19vzc/src/index.js
@@ -1,106 +1,28 @@
-const cv = document.querySelector('#canvas1');
-const ctx = cv.getContext('2d');
-const cv_H = (ctx.canvas.height = 400);
-const cv_W = (ctx.canvas.width = 400);
-const rect = cv.getBoundingClientRect();
 
-const counter = document.getElementById('clickCount');
-let clicks = 0;
-counter.innerText = clicks;
+// Spiel Konstanten
+const CV = document.querySelector('#canvas1');
+const CTX = CV.getContext('2d');
+const DISPLAY  = 400;
+const CV_RECT = CV.getBoundingClientRect();
+const COUNT = 4; // Anzahl Zeilen und Spalten
+const COUNTSTONES = COUNT * COUNT
+const STONE_SIZE = DISPLAY / COUNT
+
+CTX.canvas.height = DISPLAY
+CTX.canvas.width = DISPLAY
+
+// Statistik Konstanten
 let startTimer = true;
+let clicks = 0;
+let sort = false;
+const counter = document.getElementById('clickCount');
+counter.innerText = clicks;
 
 const newPlay = document.getElementById('newPlay');
 const clickTimer = document.getElementById('clickTimer');
 const newSort = document.getElementById('newSort');
 const resolve = document.getElementById('resolve');
-let sort = false;
 
-class Rectangle {
-  constructor(x, y, w, h, color, nr, pos) {
-    this.x = x;
-    this.y = y;
-    this.w = w;
-    this.h = h;
-    this.color = color;
-    this.nr = nr;
-    this.pos = pos;
-  }
-  draw() {
-    ctx.fillStyle = this.color;
-    ctx.strokeStyle = '#000000';
-    ctx.fillRect(this.x, this.y, this.w, this.h);
-    ctx.strokeRect(this.x + 1, this.y + 1, this.w - 1, this.h - 1);
-    ctx.beginPath();
-    ctx.font = '30px Arial';
-    ctx.fillStyle = 'gold';
-    ctx.textAlign = 'center';
-    ctx.fillText(this.nr, this.x + this.w / 2, this.y + this.h / 2 + 15);
-  }
-  update() {}
-}
-
-function randomNr() {
-  const arrNr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
-  const ar = [];
-  while (arrNr.length > 0) {
-    let ri = Math.floor(Math.random() * arrNr.length);
-    ar.push(arrNr[ri]);
-    arrNr.splice(ri, 1);
-  }
-  return ar;
-}
-
-const count = 4;
-let recs = [];
-function readRecs() {
-  let nums = [];
-  if (!sort) {
-    nums = randomNr();
-  } else {
-    nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
-  }
-
-  let x = 0;
-  let y = 0;
-  let crl = 'red';
-  let nr = 0;
-  for (let i = 0; i < nums.length; i++) {
-    nr = nums[i];
-    let mod = i % count;
-    x = mod * 100;
-    if (mod === 0) {
-      x = 0;
-      y += 100;
-    }
-    crl = nums[i] % 2 === 0 ? 'red' : 'white';
-    if (nr === 16) {
-      crl = 'black';
-      nr = '';
-    }
-    let o = { x: x, y: y - 100, nr: nr, color: crl, pos: i };
-    recs.push(o);
-  }
-}
-
-let stones = [];
-function readAllRect() {
-  readRecs();
-  let stone = null;
-  recs.forEach((r) => {
-    stone = new Rectangle(r.x, r.y, 100, 100, r.color, r.nr, r.pos);
-    stones.push(stone);
-  });
-  animate(stones);
-}
-readAllRect();
-function animate(sarr) {
-  ctx.clearRect(0, 0, cv_W, cv_H);
-  sarr.forEach((rs) => {
-    rs.draw();
-  });
-
-  // window.requestAnimationFrame(animate)
-}
 let timerRef;
 function clTimer() {
   let s = 0;
@@ -109,6 +31,98 @@ function clTimer() {
     s++;
   }, 1000);
 }
+
+class Rectangle {
+  constructor(x, y, w, h, color, nr, pos, fColor = 'gold') {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.color = color;
+    this.nr = nr;
+    this.pos = pos;
+    this.fontColor = fColor
+    this.line = 2
+    this.fontSize = Math.floor(this.w / 3)
+  }
+  draw() {
+    CTX.fillStyle = this.color;
+    CTX.strokeStyle = '#cccccc';
+    CTX.lineWidth = this.line
+    CTX.fillRect(this.x + this.line, this.y + this.line, this.w - this.line, this.h - this.line);
+    CTX.strokeRect(this.x + this.line, this.y + this.line, this.w - this.line, this.h - this.line);
+    CTX.beginPath();    
+    CTX.font = `${this.fontSize}px Arial`;   
+    CTX.fillStyle = this.fontColor;
+    CTX.textAlign = 'center';
+    CTX.fillText(this.nr, this.x + this.w / 2, this.y + this.h / 2 + (this.fontSize / 2));
+  } 
+}
+// Zufälliges Array erstellen mit Zahlen zwischen 1 und Anzahl Steine
+function randomNr() {
+  const arrNr = [];  
+  for (let i = 0; i < COUNTSTONES; i++) {
+    arrNr.push(i + 1)
+  }
+  const ar = [];
+  while (arrNr.length > 0) {
+    let ri = Math.floor(Math.random() * arrNr.length);
+    ar.push(arrNr[ri]);
+    arrNr.splice(ri, 1);
+  }
+  return ar;
+}
+// Steine erstellen
+let stones = [];
+function readRecs(arrStones) {
+  let nums = arrStones; 
+  let x = 0;
+  let y = 0;
+  let clr = 'red';
+  let fclr = 'gold'
+  let nr = 0;  
+  let stone = null
+
+  for (let i = 0; i < nums.length; i++) {
+    nr = nums[i];
+    let mod = i % COUNT;
+    x = mod * STONE_SIZE;    
+    if (mod === 0) {
+      x = 0;
+      y += STONE_SIZE;
+    }
+    clr = nums[i] % 2 === 0 ? 'red' : 'white';
+    if (nr === 16) {
+      clr = 'black';
+      fclr = 'black'
+    } else {
+      fclr = 'gold'
+    }
+    stone = new Rectangle(
+      x,
+      y - STONE_SIZE,
+      STONE_SIZE,
+      STONE_SIZE,
+      clr,
+      nr,
+      i,
+      fclr
+    ) 
+    stones.push(stone);
+  }
+}
+
+// Steine zeichnen
+function readAllRect(arrStones) {
+  CTX.clearRect(0, 0, DISPLAY, DISPLAY);
+  readRecs(arrStones); 
+  stones.forEach((r) => {
+    r.draw()
+  });
+}
+
+
+
 function clearDisplay() {
   clicks = 0;
   counter.innerText = 0;
@@ -135,10 +149,11 @@ function playCheck(arrStones) {
   })
   alert('bravo')
 }
-
-cv.addEventListener('mousedown', (e) => {
-  const mx = e.clientX - rect.left;
-  const my = e.clientY - rect.top;
+// Spiel ablauf
+readAllRect(randomNr());
+CV.addEventListener('mousedown', (e) => {
+  const mx = e.clientX - CV_RECT.left;
+  const my = e.clientY - CV_RECT.top;
   if (startTimer) {
     startTimer = false;
     clTimer();
@@ -158,11 +173,13 @@ cv.addEventListener('mousedown', (e) => {
     clicks++;
     stones[pA].nr = nStones[pF].nr;
     stones[pA].color = nStones[pF].color;
+    stones[pA].fontColor = nStones[pF].fontColor;
     stones[pF].nr = nStones[pA].nr;
     stones[pF].color = nStones[pA].color;
+    stones[pF].fontColor = nStones[pA].fontColor;
   }
 
-  animate(stones);
+
   playCheck(stones)
   counter.innerText = clicks;
 });
@@ -174,10 +191,10 @@ newSort.addEventListener('click', (e) => {
   sort = true;
   clearDisplay();
   sort = false;
-  animate(stones)
+
   playCheck(stones)
 });
 resolve.addEventListener('click', (e) => {
   kiResolve();
 });
-animate(stones);
+
